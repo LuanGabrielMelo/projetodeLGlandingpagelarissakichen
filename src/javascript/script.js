132ed51111cc193baf03a6908165de6da871fac6
Luan Gabriel <luansantomelo@gmail.com>
2025-10-01 14:06:44 -0300

    subindo a aula de landing page de Larissa kich

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,83 +1,60 @@
-$(document).ready(function(){
-    $('#mobile_btn').on('click', function(){
+$(document).ready(function() {
+    $('#mobile_btn').on('click', function () {
         $('#mobile_menu').toggleClass('active');
-        $('#mobile_btn').find('i').toggleClass(' fa-x');
+        $('#mobile_btn').find('i').toggleClass('fa-x');
     });
-     
-        const  section = $('section');
-        const navitems =$('nav_item');
 
-    $(window).on('scroll',function(){
-        const header = $('header')
-        const scrollPosition =  $(window).scrolltop()- header.outerheight( );
+    const sections = $('section');
+    const navItems = $('.nav-item');
+
+    $(window).on('scroll', function () {
+        const header = $('header');
+        const scrollPosition = $(window).scrollTop() - header.outerHeight();
+
         let activeSectionIndex = 0;
-        
-        if(srollpodition<=0){
-            header.css('box-shadow','none');
-        }else{
-            header.css('box-shadow','5px 1px 5px rgba(0,0,0,0.1)');
+
+        if (scrollPosition <= 0) {
+            header.css('box-shadow', 'none');
+        } else {
+            header.css('box-shadow', '5px 1px 5px rgba(0, 0, 0, 0.1');
         }
-        section.each(function(i){
+
+        sections.each(function(i) {
             const section = $(this);
             const sectionTop = section.offset().top - 96;
-            const sectionBottom = sectionTop+ section.outerheight();
+            const sectionBottom = sectionTop+ section.outerHeight();
 
-            if(scrollPosition>=sectionTop&&scrollPosition<sectionBottom){
+            if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
                 activeSectionIndex = i;
                 return false;
             }
         })
-        navitems.removeClass('active');
-        $(navitems[activeSectionIndex]).addClass('active');
 
-    });
-
-    scrollReveal().reveal('#cta',{
-         origin: 'left',
-         duration: 2000,
-         distance: '20%'
-    });
-     scrollReveal().reveal('.dish',{
-         origin: 'left',
-         duration: 2000,
-         distance: '20%'
-    });
-     scrollReveal().reveal('#testemonials_chef',{
-         origin: 'left',
-         duration: 1000,
-         distance: '20%'
-    });
-     scrollReveal().reveal('.feedback',{
-         origin: 'rigth',
-         duration: 1000,
-         distance: '20%'
+        navItems.removeClass('active');
+        $(navItems[activeSectionIndex]).addClass('active');
     });
 
+    ScrollReveal().reveal('#cta', {
+        origin: 'left',
+        duration: 2000,
+        distance: '20%'
+    });
+
+    ScrollReveal().reveal('.dish', {
+        origin: 'left',
+        duration: 2000,
+        distance: '20%'
+    });
+
+    ScrollReveal().reveal('#testimonial_chef', {
+        origin: 'left',
+        duration: 1000,
+        distance: '20%'
+    })
+
+    ScrollReveal().reveal('.feedback', {
+        origin: 'right',
+        duration: 1000,
+        distance: '20%'
+    })
 });
-/*
-função do js para fazer animacão de rolagem do scroll alteran o destinatario  no menu header 
-$(window).on('scroll',function(){
-        const header = $('header')
-        const scrollPosition =  $(window).scrolltop()- header.outerheight( );
-        let activeSectionIndex = 0;
-        
-        if(srollpodition<=0){
-            header.css('box-shadow','none');
-        }else{
-            header.css('box-shadow','5px 1px 5px rgba(0,0,0,0.1)');
-        }
-        section.each(function(i){
-            const section = $(this);
-            const sectionTop = section.offset().top - 96;
-            const sectionBottom = sectionTop+ section.outerheight();
-
-            if(scrollPosition>=sectionTop&&scrollPosition<sectionBottom){
-                activeSectionIndex = i;
-                return false;
-            }
-        })
-        navitems.removeClass('active');
-        $(navitems[activeSectionIndex]).addClass('active');
-
-    });
-    */
